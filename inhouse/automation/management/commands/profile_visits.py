5af528a3e974541f07abbdfe1260857269484f81
rakesh2884 <rakebist@gmail.com>
2024-05-06 12:41:05 +0530

    profile_visits

--- inhouse/automation/management/commands/profile_visits.py
+++ inhouse/automation/management/commands/profile_visits.py
@@ -13,6 +13,7 @@ from django.core.management.base import BaseCommand
 logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
+
 class Command(BaseCommand):
     help = 'scrapping the Linkedin Jobs'
 
@@ -22,8 +23,6 @@ class Command(BaseCommand):
             self.login_to_linkedin(driver)
             for keyword in keywords:
                 self.search_keyword_and_visit_profiles(driver, keyword)
-                # Reset visit_count for the next keyword search
-                visit_count = 0
 
     def login_to_linkedin(self, driver):
         driver.get("https://www.linkedin.com/login")
@@ -47,39 +46,47 @@ class Command(BaseCommand):
     def search_keyword_and_visit_profiles(self, driver, keyword):
         search_url = f"https://www.linkedin.com/search/results/people/?keywords={keyword}"
         driver.get(search_url)
-
         try:
             WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "search-results-container")))
 
             visit_count = 0
             next_count = 0
-            while visit_count < 10: 
-                try:  
+            while visit_count <= 10: 
+                try: 
                     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                    time.sleep(5)            
-                    links = driver.find_elements(By.XPATH, "//a[@class='app-aware-link ']//span[@dir='ltr']//span[@aria-hidden='true']")   
-                    for i in range(len(links)):
-                        try:
-                            ActionChains(driver).click(links[visit_count]).perform()
-                            time.sleep(5)
-                            visit_count += 1
-                            if visit_count >= 10 and next_count < 9:
-                                next_button = driver.find_element(By.ID, "ember634")
+                    time.sleep(5)         
+                    links = driver.find_elements(By.XPATH, "//a[@class='app-aware-link ']//span[@dir='ltr']//span[@aria-hidden='true']") 
+                    if visit_count != len(links):  
+                        for i in range(len(links)):
+                            try:
+                                ActionChains(driver).click(links[visit_count]).perform()
+                                time.sleep(5)
+                                visit_count += 1
+                                break
+                            except StaleElementReferenceException:
+                                logging.warning("StaleElementReferenceException occurred. Trying to re-find the elements.")
+                                break         
+                            except NoSuchElementException:
+                                logging.warning("Follow button found but not clickable. Moving to the next button.")
+                            finally:
+                                driver.back()
+                                WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "search-results-container")))
+                    else:
+                        if next_count < 9:
+                            try:
+                                next_button = WebDriverWait(driver, 10).until(
+                                    EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Next']"))
+                                )
                                 next_button.click()
                                 next_count += 1
                                 visit_count = 0
+                                continue
+                            except NoSuchElementException:
+                                print("Next button not found or timed out.")
                                 break
+                        else:
                             break
-                        except StaleElementReferenceException:
-                            logging.warning("StaleElementReferenceException occurred. Trying to re-find the elements.")
-                            break         
-                        except NoSuchElementException:
-                            logging.warning("Follow button found but not clickable. Moving to the next button.")
-                        finally:
-                            driver.back()
-                            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "search-results-container")))
                 except NoSuchElementException:
                     logging.warning("Error while scrolling. Moving to the next scroll.")
         except TimeoutException:
             logging.warning("TimeoutException: Element 'search-results-container' not found for keyword:", keyword)
-            return 
